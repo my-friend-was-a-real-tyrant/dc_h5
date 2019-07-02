41bccd4d6538f1026be21c2a14983d8660689dfb
胡李斌 <hulibin@hulibindeMacBook-Pro.local>
2019-07-02 22:31:27 +0800

    repack

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,9 @@ export default {
 <style lang="less">
 html,body{
   width:100%;
-  min-height:100%;
-  height: auto;
+  height: 100%;
   margin:0;
-  position: relative;
-  bottom: 0;
+  position: absolute;
   padding:0;
   *{
     font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
